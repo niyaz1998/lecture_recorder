edeb39eab19f31b9741ef5740805106ec6e3f979
cepuchino <niyaz1998@yandex.ru>
2020-03-23 20:37:05 +0300

    added timer

--- app/src/main/java/com/example/lecturerecorder/recorder_activity/RecorderActivity.kt
+++ app/src/main/java/com/example/lecturerecorder/recorder_activity/RecorderActivity.kt
@@ -4,10 +4,12 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.lecturerecorder.R
 import kotlinx.android.synthetic.main.activity_recorder.*
+
 
 private const val LOG_TAG = "RecorderActivity"
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
@@ -21,9 +23,11 @@ class RecorderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recorder)
 
-        viewModel = RecorderViewModel("${externalCacheDir?.absolutePath}/audiorecordtest.3gp")
+        val fileName = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
 
-        Log.e("recorder path", "${externalCacheDir?.absolutePath}/audiorecordtest.3gp\"")
+        viewModel = RecorderViewModel(fileName, this)
+
+        Log.e("recorder path", fileName)
         bMain.setOnClickListener {
             viewModel?.onMainButtonPressed()
         }
@@ -60,4 +64,30 @@ class RecorderActivity : AppCompatActivity() {
 
         viewModel?.onStop()
     }
+
+    fun setTime(millis: Long) {
+        runOnUiThread {
+            val seconds = (millis / 1000).toInt()
+            val minutes = seconds / 60
+            val hours = minutes / 60
+
+            val text = "${formatForTime(hours)}:${formatForTime(minutes)}:${formatForTime(seconds)}"
+
+            textTimer.text = text
+        }
+    }
+
+    private fun formatForTime(value: Int): String {
+        return if (value < 10) "0${value}" else value.toString()
+    }
+
+    fun setButton(state: ButtonState) {
+        when (state) {
+            ButtonState.RECORD -> bMain.setImageResource(R.drawable.ic_radio)
+            ButtonState.STOP_RECORD -> bMain.setImageResource(R.drawable.ic_mute)
+            ButtonState.NULL -> bMain.visibility = View.GONE
+        }
+    }
 }
+
+enum class ButtonState { RECORD, STOP_RECORD, NULL }
--- app/src/main/java/com/example/lecturerecorder/recorder_activity/RecorderViewModel.kt
+++ app/src/main/java/com/example/lecturerecorder/recorder_activity/RecorderViewModel.kt
@@ -3,24 +3,41 @@ package com.example.lecturerecorder.recorder_activity
 import android.media.MediaRecorder
 import android.util.Log
 import java.io.IOException
+import java.util.*
 
 
 enum class RecorderState { NOT_STARTED, RECORDING, RECORDED }
 
 private const val LOG_TAG = "RecorderViewModel"
 
-class RecorderViewModel(private val fileName: String) {
+class RecorderViewModel(private val fileName: String, private val view: RecorderActivity) {
     private var recorder: MediaRecorder? = null
     private var state: RecorderState = RecorderState.NOT_STARTED
+    private var timer: Timer? = null
+
+    init {
+        view.setButton(ButtonState.RECORD)
+    }
 
     fun onMainButtonPressed() {
         if (state == RecorderState.NOT_STARTED) {
             startRecording()
+            startTimer()
             state = RecorderState.RECORDING
+            view.setButton(ButtonState.STOP_RECORD)
         } else if (state == RecorderState.RECORDING) {
             stopRecording()
+            stopTimer()
             state = RecorderState.RECORDED
+            view.setButton(ButtonState.NULL)
         }
+    }
+
+    fun onStop() {
+        recorder?.stop()
+        recorder?.reset()
+        recorder?.release()
+        recorder = null
     }
 
     private fun startRecording() {
@@ -49,10 +66,27 @@ class RecorderViewModel(private val fileName: String) {
         recorder = null
     }
 
-    fun onStop() {
-        recorder?.stop()
-        recorder?.reset()
-        recorder?.release()
-        recorder = null
+    private fun startTimer() {
+        timer = Timer()
+        timer?.schedule(MillisPassedTimer(Calendar.getInstance()) {
+            view.setTime(it)
+        }, 0, 1000)
+    }
+
+    private fun stopTimer() {
+        timer?.cancel()
+        timer = null
+        view.setTime(0)
+    }
+}
+
+class MillisPassedTimer(
+    private val time: Calendar,
+    private val callback: (Seconds: Long) -> Unit
+) :
+    TimerTask() {
+
+    override fun run() {
+        callback((Calendar.getInstance().timeInMillis - time.timeInMillis))
     }
 }
