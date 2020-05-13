86995c8d8326de827b33c8c547a203b1ed32bd8d
cepuchino <niyaz1998@yandex.ru>
2020-05-13 15:21:43 +0300

    basic music playing

--- app/src/main/java/com/example/lecturerecorder/player_activity/ListenerActivity.kt
+++ app/src/main/java/com/example/lecturerecorder/player_activity/ListenerActivity.kt
@@ -1,12 +1,14 @@
 package com.example.lecturerecorder.player_activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lecturerecorder.R
 import com.example.lecturerecorder.model.LectureRecord
 import com.example.lecturerecorder.model.Note
 import com.example.lecturerecorder.model.ReadNotesAdapter
+import com.example.lecturerecorder.utils.formatTime
 import kotlinx.android.synthetic.main.activity_listener.*
 
 var stubLecture = LectureRecord(
@@ -19,7 +21,7 @@ var stubLecture = LectureRecord(
         Note(seconds = 50, text = "note 5"),
         Note(seconds = 60, text = "note 6")
     ),
-    fileLocation = ""
+    fileLocation = "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_2MG.mp3"
 )
 
 class ListenerActivity : AppCompatActivity() {
@@ -29,6 +31,7 @@ class ListenerActivity : AppCompatActivity() {
     }
 
     lateinit var lectureRecord: LectureRecord
+    lateinit var listenerViewModel: ListenerViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +41,37 @@ class ListenerActivity : AppCompatActivity() {
             if (intent.extras?.getParcelable<LectureRecord>(ARGUMENTS) != null)
                 intent.extras?.getParcelable(ARGUMENTS)!! else stubLecture
 
+        listenerViewModel = ListenerViewModel(
+            lectureRecord,
+            this,
+            seekBar
+        )
+
         rv_read_notes.apply {
             layoutManager = LinearLayoutManager(this@ListenerActivity)
             adapter = ReadNotesAdapter(lectureRecord.notes)
         }
+
+        bMain.setOnClickListener {
+            listenerViewModel.onButtonPressed()
+        }
+    }
+
+    fun setButtonState(state: MainButtonState) {
+        when (state) {
+            MainButtonState.PLAYING -> bMain.setImageResource(R.drawable.ic_stop)
+            MainButtonState.PAUSED -> bMain.setImageResource(R.drawable.ic_play_circle)
+            // MainButtonState.NULL -> bMain.visibility = View.INVISIBLE
+            MainButtonState.STOPPED -> bMain.setImageResource(R.drawable.ic_play_circle)
+        }
+    }
+
+    fun setTime(seconds: Int) {
+        tvTime.text = formatTime(seconds = seconds.toLong())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        listenerViewModel.stopPlaying()
     }
 }
